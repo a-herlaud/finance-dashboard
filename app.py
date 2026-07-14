026ab20e2e645af838bce9d709a8f99157a32447
aherlaud <aherlaud@student.42.fr>
2026-07-14 16:21:34 +0200

    feat(index page): loop through the indexes to have them all analized in one page

--- app.py
+++ app.py
@@ -1,106 +1,26 @@
 import streamlit as st
-import plotly.express as px
 import pandas as pd
 import yfinance as yf
 from components.sidebar import render_sidebar
-from datetime import datetime
+from components.full_index import full_index
 
-HEIGHT = 500
+indexes = {
+    "S&P 500": "^GSPC",
+    "NASDAQ 100": "^NDX",
+    "Dow Jones": "^DJI",
+    "CAC 40": "^FCHI",
+    "Nikkei 225": "^N225",
+    "KOSPI": "^KS11",
+    "DAX 40": "^GDAXI",
+    "FTSE 100": "^FTSE",
+}
 
 st.set_page_config(page_title="Finance Dashboard", layout="wide")
 
 st.title("FINANCE DASHBOARD")
 
-settings = render_sidebar()
+render_sidebar()
 
-df = yf.download(
-    "^GSPC",
-    period="5y",
-    interval="1d",
-    auto_adjust=True,
-    progress=False,
-)
-
-# Remove the ticker level
-df.columns = df.columns.get_level_values(0)
-
-# Make Date a normal column
-df = df.reset_index()
-
-def get_period_prices(prices, days):
-    return prices.iloc[-days:].dropna()
-
-def get_period_returns(prices):
-    return prices.pct_change().dropna()
-
-def simple_return(prices):
-    return prices.iloc[-1] / prices.iloc[0] - 1
-
-def cumulative_return(returns):
-    return (1 + returns).prod() - 1
-
-def annualized_return(returns):
-    total_return = (1 + returns).prod()
-    years = len(returns) / 252
-    return total_return ** (1 / years) - 1
-
-periods = {
-    "1D": 1,
-    "1M": 21,
-    "YTD": (datetime.now().date() - datetime(datetime.now().year,1,1).date()).days,
-    "1Y": 252,
-    "5Y": 252*5
-}
-
-metrics = {}
-
-for name, days in periods.items():
-    prices = get_period_prices(df['Close'], days)
-    r = get_period_returns(prices)
-
-    metrics[name] = {
-        "Simple return": simple_return(prices),        
-        "Cumulative return": cumulative_return(r),
-        "Open": prices.iloc[0],
-        "Close": prices.iloc[-1],
-    }
-
-df_metrics = pd.DataFrame(metrics)
-
-
-
-
-fig = px.line(
-    df,
-    x="Date",
-    y="Close",
-)
-
-fig.update_layout(
-    xaxis_title="Date",
-    yaxis_title="Price (USD)",
-    template="plotly_white",
-)
-
-head1, head2 = st.columns(2)
-with head1:
-    st.header("S&P 500")
-
-with head2:
-    price = get_period_prices(df["Close"], 2)
-    daily_return = (price.iloc[0] - price.iloc[1]) / price.iloc[0]
-    st.metric(
-        label="Daily Return",
-        value=f"{price.iloc[0]:.6}$",
-        delta=f"{daily_return:.2%}",
-    )
-
-col1, col2 = st.columns(2)
-with col1:
-    with st.container(border=True, height=HEIGHT):
-        st.dataframe(df_metrics, height=HEIGHT, width='stretch')
-
-with col2:
-    with st.container(border=True, height=HEIGHT):
-        fig.update_layout(height=HEIGHT)
-        st.plotly_chart(fig, width='stretch')
+for name, ticker in indexes.items():
+    st.divider()
+    full_index(ticker, name)
